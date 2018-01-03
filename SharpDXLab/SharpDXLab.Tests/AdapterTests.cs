72b80f0ba58e2572c871108c8fb2cc4fed6a5e21
Jérémy Ansel <JeremyAnsel@users.noreply.github.com>
2018-01-03 21:09:22 +0100

    Update tests

--- SharpDXLab/SharpDXLab.Tests/AdapterTests.cs
+++ SharpDXLab/SharpDXLab.Tests/AdapterTests.cs
@@ -19,14 +19,16 @@ namespace SharpDXLab.Tests
                 foreach (var item in factory.Adapters)
                 {
                     var description = item.Description;
+                    var level = SharpDX.Direct3D11.Device.GetSupportedFeatureLevel(item);
 
                     var sb = new StringBuilder();
+                    sb.AppendLine(description.Description);
                     sb.AppendLine("\tVendorId: 0x" + description.VendorId.ToString("X"));
                     sb.AppendLine("\tDeviceId: 0x" + description.DeviceId.ToString("X"));
-                    sb.AppendLine(description.Description);
                     sb.AppendLine("\tDedicatedVideoMemory: " + description.DedicatedVideoMemory);
                     sb.AppendLine("\tDedicatedSystemMemory: " + description.DedicatedSystemMemory);
                     sb.AppendLine("\tSharedSystemMemory: " + description.SharedSystemMemory);
+                    sb.AppendLine("\tFeatureLevel: " + level);
                     Console.Write(sb);
                 }
             }
