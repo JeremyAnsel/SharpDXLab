8213e3aaf7ffb420380c526d3b7aea37daec25f3
Jérémy Ansel <JeremyAnsel@users.noreply.github.com>
2020-04-18 16:28:25 +0200

    Create binary file

--- SharpDXLab/SharpDXLab.Tests/Class1Tests.cs
+++ SharpDXLab/SharpDXLab.Tests/Class1Tests.cs
@@ -1,6 +1,7 @@
 ﻿using NUnit.Framework;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,6 +17,14 @@ namespace SharpDXLab.Tests
             string hello = Class1.HelloWorld();
             Assert.AreEqual("Hello, World!", hello);
             Console.WriteLine(hello);
+        }
+
+        [Test]
+        public void CreateBinaryFileTest()
+        {
+            Directory.CreateDirectory("images");
+            var data = new byte[1];
+            File.WriteAllBytes("images\\file.bin", data);
         }
     }
 }
